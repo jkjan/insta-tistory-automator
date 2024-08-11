da927e71b7ab3f4daf60946cda0e5a5e19780001
jkjan <ytom2607@gmail.com>
2024-08-11 22:58:41 +0900

    initial commit

---
+++ InstaTistoryAutomatorServer/src/test/kotlin/com/jun/instatistoryautomatorserver/InstaTistoryAutomatorServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jun.instatistoryautomatorserver
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InstaTistoryAutomatorServerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
